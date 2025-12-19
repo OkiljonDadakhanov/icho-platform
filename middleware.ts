0793f8938c5de6bb02e52e1b7dab273e4f234c4b
Okiljon <okiljondadakhonov@gmail.com>
2025-12-19 17:29:04 +0500

    upd

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,5 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const blockedRoutes = ["/", "/", "/"];
-
-  if (blockedRoutes.includes(req.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL("/updating", req.url));
-  }
-
   return NextResponse.next();
 }
-
-// export const config = {
-//   matcher: ["/documents", "/invitations", "/travel"],
-// };
